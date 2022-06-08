f664a424a7e8c97a1687f4b2a5b705625f5b3a7e
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-06-08 17:18:00 +0530

    Some Deployment issue

--- reacttest/src/Components/Card.jsx
+++
@@ -1,9 +0,0 @@
-
-export const Card = ({title,body}) => {
-  return (
-    <div className='card-c'>
-        <h2 className='tit'>{title}</h2>
-        <p className='body'>{body}</p>
-    </div>
-  );
-};
