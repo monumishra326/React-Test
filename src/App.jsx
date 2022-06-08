1330586f2a888173f69ebdd2a731f8f4f58b76f3
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-06-08 17:19:28 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Components/Post"
+import './App.css'
+
+
+ function App() {
+  return (
+    <div>
+      <Post/>
+    </div>
+  );
+}
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+
+export const Card = ({title,body}) => {
+  return (
+    <div className='card-c'>
+        <h2 className='tit'>{title}</h2>
+        <p className='body'>{body}</p>
+    </div>
+  );
+};
